daf0440591657b9868b5f622914951f7a0de0d57
ScoutRishabh <rishabh@scoutbetter.com>
2025-02-21 12:32:35 +0530

    first commit

--- src/components/PDFDownloadButton.tsx
+++ src/components/PDFDownloadButton.tsx
@@ -3,7 +3,7 @@
 import type React from "react"
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
-import type { jsPDF } from 'jspdf'
+import { jsPDF } from 'jspdf'
 import 'jspdf-autotable'
 
 // Add this to extend jsPDF type with autoTable
